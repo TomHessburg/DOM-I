e7da33e0ebf161b911e29cbe88029668a506a5dd
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-01-28 16:57:32 -0500

    counter is counting on stretch assignment, need to make it stop at 10 seconds

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,51 @@
+const secondTens = document.getElementById("secondTens");
+const secondOnes = document.getElementById("secondOnes");
+const msHundreds = document.getElementById("msHundreds");
+const msTens = document.getElementById("msTens");
+
+
+
+function myTimerFunc(){
+    
+        //tens dosnt need to be elastic because
+    setInterval(function(){ 
+        secondTens.innerHTML = 1;
+    }, 10000);
+
+
+        //ones  
+    let countMe = 1;
+    setInterval(function(){ 
+        secondOnes.innerHTML = countMe;
+        countMe++;
+        if(countMe=== 10){
+            countMe=0;
+        }
+    }, 1000);
+
+        //hundreds
+    let countMeHundreds = 1;
+    setInterval(function(){ 
+        msHundreds.innerHTML = countMeHundreds;
+        countMeHundreds++;
+        if(countMeHundreds=== 10){
+            countMeHundreds=0;
+        }
+    }, 100);
+
+        //thousands
+    let countMeThousand = 1;
+    setInterval(function(){ 
+        msTens.innerHTML = countMeThousand;
+        countMeThousand++;
+        if(countMeThousand=== 10){
+            countMeThousand=0;
+        }
+    }, 10);
+
+}
+
+
+
+
+myTimerFunc();
