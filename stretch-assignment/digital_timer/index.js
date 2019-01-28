5a72ede0357896f407197cff644c97b429fd7f21
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-01-28 17:33:59 -0500

    stretch complete

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -8,45 +8,88 @@ const msTens = document.getElementById("msTens");
 function myTimerFunc(){
     
         //tens dosnt need to be elastic because
-    setInterval(function(){ 
-        secondTens.innerHTML = 1;
+    let tensCounter = 0;
+    let tenSecondsFunc = function(){ 
+            tensCounter = 1;
+            secondTens.innerHTML = tensCounter;
+    
+            secondTens.classList.add("redDigit")
+            secondOnes.classList.add("redDigit")
+            msHundreds.classList.add("redDigit")
+            msTens.classList.add("redDigit");
 
-        secondTens.classList.add("redDigit")
-        secondOnes.classList.add("redDigit")
-        msHundreds.classList.add("redDigit")
-        msTens.classList.add("redDigit")
-    }, 10000);
+            if(tensCounter === 1){
+                clearInterval(init1);
+            }
+    }
+
+    let init1 = setInterval(tenSecondsFunc, 10000);
+
+
 
 
         //ones  
     let countMe = 1;
-    setInterval(function(){ 
+    let secondsFunc = function(){ 
         secondOnes.innerHTML = countMe;
         countMe++;
-        if(countMe=== 10){
+        if(countMe === 10){
             countMe=0;
         }
-    }, 1000);
+        if(tensCounter === 1){
+            clearInterval(init2);
+        }
+        
+    }
+    let init2 = setInterval(secondsFunc, 1000);
+
+
+
 
         //hundreds
-    let countMeHundreds = 1;
-    setInterval(function(){ 
+    let hundredsFunc = function(){ 
         msHundreds.innerHTML = countMeHundreds;
         countMeHundreds++;
         if(countMeHundreds=== 10){
             countMeHundreds=0;
         }
-    }, 100);
+        if(tensCounter === 1){
+            clearInterval(init3);
+        }
+    }    
+    let countMeHundreds = 1;
+    let init3 = setInterval(hundredsFunc, 100);
 
+
+    
         //thousands
     let countMeThousand = 1;
-    setInterval(function(){ 
+    let thousandsFunc = function(){ 
         msTens.innerHTML = countMeThousand;
         countMeThousand++;
         if(countMeThousand=== 10){
             countMeThousand=0;
         }
-    }, 10);
+        if(tensCounter === 1){
+            clearInterval(init4);
+        }
+    }
+    let init4 = setInterval(thousandsFunc, 10);
+
+
+
+
+        //clearing intervals
+    // function myStopFunction() {
+        // clearInterval(tenSecondsFunc);
+        // clearInterval(secondsFunc);
+        // clearInterval(hundredsFunc);
+        // clearInterval(thousandsFunc);
+    // }
+
+    // if(secondTens.innerHTML === 1){
+    //     myStopFunction() ;
+    // }
 
 
 }
